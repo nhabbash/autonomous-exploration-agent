678c79435dcff325b18f344565960baa10148f2b
federicobottoni2 <feder.bottoni@gmail.com>
2020-09-02 14:30:50 +0200

    build, deploy

---
+++ webapp/build/precache-manifest.4c3de588c5931164bf20556a8a8ccd9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1031e3ed052f460c19bb446b701c8e",
+    "url": "/autonomous-exploration-agent/index.html"
+  },
+  {
+    "revision": "0060a1124fff0e27e842",
+    "url": "/autonomous-exploration-agent/static/css/main.0eb3a2e0.chunk.css"
+  },
+  {
+    "revision": "59853dfcb5728f583b67",
+    "url": "/autonomous-exploration-agent/static/js/2.ffef0d36.chunk.js"
+  },
+  {
+    "revision": "83af3834c661e921ae64d25afaae3aaa",
+    "url": "/autonomous-exploration-agent/static/js/2.ffef0d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0060a1124fff0e27e842",
+    "url": "/autonomous-exploration-agent/static/js/main.36861422.chunk.js"
+  },
+  {
+    "revision": "cb3470af3eac22499df6",
+    "url": "/autonomous-exploration-agent/static/js/runtime-main.dcea75b2.js"
+  }
+]);
--- webapp/build/service-worker.js
+++ webapp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04091c1c1a32f9b52cd8d3f522d9d09d.js"
+  "/autonomous-exploration-agent/precache-manifest.4c3de588c5931164bf20556a8a8ccd9c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/autonomous-exploration-agent/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
